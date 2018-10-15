89d6c0d403c73ab5298a64b4b1a04abed88945fd
Laura Hoffman <lhoffmanwg@gmail.com>
2018-10-15 04:46:11 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+var recipes={};
+function updateObjectWithKeyAndValue(obj, key, value){
+  return Object.assign({}, obj, { [key]: value });
+}
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
+  obj[key] = value;
+  return obj;
+}
+function deleteFromObjectByKey(obj, key){
+  
+}
